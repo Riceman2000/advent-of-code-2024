444d3bcf9fbb6d7f4b3dbd2d7f58f63fad4473d7
alex.rice <alex.rice@kyrus-tech.com>
2024-12-05 02:48:26 -0500

    Change input parsing

--- src/day05_1.rs
+++ src/day05_1.rs
@@ -1,49 +1,50 @@
-use itertools::Itertools;
+use atoi::atoi;
 use std::cmp::Ordering;
 
 // Pull this file's contents into the binary as a string literal
-const INPUT: &str = include_str!("../input/day05.txt");
+const INPUT: &[u8] = include_bytes!("../input/day05.txt");
 
 #[must_use]
 #[allow(clippy::missing_panics_doc)]
 pub fn day() -> u32 {
-    let lines: Vec<_> = INPUT.trim_ascii_end().lines().collect();
+    let lines: Vec<_> = INPUT.trim_ascii_end().split(|c| *c == b'\n').collect();
     let section_split = lines.iter().position(|l| l.is_empty()).unwrap();
     let orders: Vec<_> = lines[0..section_split]
         .iter()
-        .map(|r| {
+        .map(|r| unsafe {
             (
-                r[0..2].parse::<u8>().unwrap(),
-                r[3..].parse::<u8>().unwrap(),
+                atoi::<u8>(&r[0..2]).unwrap_unchecked(),
+                atoi::<u8>(&r[3..]).unwrap_unchecked(),
             )
         })
         .collect();
     let reports: Vec<_> = lines[section_split + 1..]
         .iter()
         .map(|r| {
-            r.split(',')
-                .map(|n| n.parse::<u8>().unwrap())
+            r.split(|c| *c == b',')
+                .map(|n| unsafe { atoi::<u8>(n).unwrap_unchecked() })
                 .collect::<Vec<u8>>()
         })
         .collect();
 
     let mut sum: u32 = 0;
-    for report in reports {
-        let mut is_bad = false;
-        let sorted: Vec<_> = report
-            .iter()
-            .sorted_by(|a, b| {
-                if orders.contains(&(**a, **b)) {
-                    is_bad = true;
-                    Ordering::Less
-                } else {
-                    Ordering::Greater
-                }
-            })
-            .collect();
-        if is_bad {
-            sum += u32::from(*sorted[sorted.len() / 2]);
+    for mut report in reports {
+        if report
+            .array_windows()
+            .all(|&[a, b]| orders.contains(&(a, b)))
+        {
+            continue;
         }
+
+        report.sort_by(|a, b| {
+            if orders.contains(&(*a, *b)) {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        });
+
+        sum += u32::from(report[report.len() / 2]);
     }
 
     sum
