32b61e8142d9f29e07df2d1cc1eea7292851c6c8
alex.rice <alex.rice@kyrus-tech.com>
2024-12-09 00:33:23 -0500

    Day 8 part 1

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ fn main() {
     total_proc += process_day("day06_1", day06_1::day, &args);
     total_proc += process_day("day07_0", day07_0::day, &args);
     total_proc += process_day("day07_1", day07_1::day, &args);
-    // total_proc += process_day("day08_0", day08_0::day, &args);
+    total_proc += process_day("day08_0", day08_0::day, &args);
     // total_proc += process_day("day08_1", day08_1::day, &args);
     // total_proc += process_day("day09_0", day09_0::day, &args);
     // total_proc += process_day("day09_1", day09_1::day, &args);
