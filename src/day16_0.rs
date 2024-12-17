a0b9c1d64b97ab916dfb235979aa6e9d1ba5e1aa
alex.rice <alex.rice@kyrus-tech.com>
2024-12-17 11:17:55 -0500

    Day 16 part 1

--- src/day16_0.rs
+++ src/day16_0.rs
@@ -1,12 +1,14 @@
+use pathfinding::directed::dijkstra;
+
 // Pull this file's contents into the binary as a string literal
-const INPUT: &[u8] = include_bytes!("../input/day16-short.txt");
+const INPUT: &[u8] = include_bytes!("../input/day16.txt");
 
 #[must_use]
 #[allow(clippy::missing_panics_doc)]
 pub fn day() -> usize {
     let mut start_pos = (0, 0);
     let mut exit_pos = (0, 0);
-    let mut grid: Vec<_> = INPUT
+    let grid: Vec<_> = INPUT
         .trim_ascii()
         .split(|c| *c == b'\n')
         .enumerate()
@@ -16,41 +18,48 @@ pub fn day() -> usize {
                 .map(|(x, b)| {
                     if *b == b'S' {
                         start_pos = (x, y);
-                        Tile::Empty
                     } else if *b == b'E' {
                         exit_pos = (x, y);
-                        Tile::Empty
-                    } else {
-                        Tile::from_byte(*b)
                     }
+                    Tile::from_byte(*b)
                 })
                 .collect::<Vec<_>>()
         })
         .collect();
 
-    pretty_print(&grid, start_pos, exit_pos);
-    0
+    let out = dijkstra::dijkstra(
+        &(start_pos.0, start_pos.1, Direction::E),
+        |&(x, y, dir): &(usize, usize, Direction)| {
+            let forward_pos = (
+                x.wrapping_add_signed(dir.to_coords().0),
+                y.wrapping_add_signed(dir.to_coords().1),
+            );
+
+            [
+                ((x, y, dir.next()), 1000),
+                ((forward_pos.0, forward_pos.1, dir), 1),
+                ((x, y, dir.prev()), 1000),
+            ]
+            .into_iter()
+            .filter(|&((x, y, _), _)| grid[y][x] != Tile::Wall)
+        },
+        |&node| node == (exit_pos.0, exit_pos.1, node.2),
+    )
+    .unwrap();
+
+    // pretty_print(&grid, &out.0);
+    out.1
 }
 
 #[allow(dead_code)]
-fn pretty_print(grid: &[Vec<Tile>], start_pos: (usize, usize), exit_pos: (usize, usize)) {
-    for (y, line) in grid.iter().enumerate() {
-        for (x, c) in line.iter().enumerate() {
-            if (x, y) == start_pos {
-                if *c == Tile::Empty {
-                    print!("S");
-                } else {
-                    print!("!");
-                }
-            } else if (x, y) == exit_pos {
-                if *c == Tile::Empty {
-                    print!("E");
-                } else {
-                    print!("X");
-                }
-            } else {
-                print!("{}", c.to_char());
-            }
+fn pretty_print(grid: &[Vec<Tile>], path: &[(usize, usize, Direction)]) {
+    let mut grid = grid.to_vec();
+    for (x, y, dir) in path {
+        grid[*y][*x] = Tile::Path(*dir);
+    }
+    for line in grid {
+        for c in line {
+            print!("{}", c.to_char());
         }
         println!();
     }
@@ -58,24 +67,71 @@ fn pretty_print(grid: &[Vec<Tile>], start_pos: (usize, usize), exit_pos: (usize,
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Tile {
+    Start,
+    Exit,
     Empty,
-    WallFresh,
-    WallVisited(u64),
+    Wall,
+    Path(Direction),
 }
 
 impl Tile {
     fn from_byte(byte: u8) -> Self {
         match byte {
+            b'S' => Tile::Start,
+            b'E' => Tile::Exit,
             b'.' => Tile::Empty,
-            b'#' => Tile::WallFresh,
+            b'#' => Tile::Wall,
             b => unreachable!("Hit char '{}'", b as char),
         }
     }
 
     fn to_char(self) -> char {
         match self {
+            Tile::Start => 'S',
+            Tile::Exit => 'E',
             Tile::Empty => ' ',
-            Tile::WallFresh | Tile::WallVisited(_) => '#',
+            Tile::Wall => '#',
+            Tile::Path(Direction::N) => '^',
+            Tile::Path(Direction::E) => '>',
+            Tile::Path(Direction::S) => 'v',
+            Tile::Path(Direction::W) => '<',
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum Direction {
+    N,
+    E,
+    S,
+    W,
+}
+
+impl Direction {
+    fn to_coords(self) -> (isize, isize) {
+        match self {
+            Direction::N => (0, -1),
+            Direction::E => (1, 0),
+            Direction::S => (0, 1),
+            Direction::W => (-1, 0),
+        }
+    }
+
+    fn next(self) -> Self {
+        match self {
+            Direction::N => Direction::E,
+            Direction::E => Direction::S,
+            Direction::S => Direction::W,
+            Direction::W => Direction::N,
+        }
+    }
+
+    fn prev(self) -> Self {
+        match self {
+            Direction::N => Direction::W,
+            Direction::E => Direction::N,
+            Direction::S => Direction::E,
+            Direction::W => Direction::S,
         }
     }
 }
