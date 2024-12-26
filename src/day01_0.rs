e2f9c22b2fe68e6d385954f98fb1d8a15c466531
Riceman2000 <alex4222000@gmail.com>
2024-12-25 22:18:24 -0500

    day1 part 1

--- src/day01_0.rs
+++ src/day01_0.rs
@@ -2,14 +2,21 @@
 const INPUT: &str = include_str!("../input/day01.txt");
 
 #[must_use]
+#[allow(clippy::missing_panics_doc)]
 pub fn day() -> u32 {
-    return INPUT
+    let (mut col1, mut col2): (Vec<u32>, Vec<u32>) = INPUT
         .lines()
         .map(|l| {
-            let line_vec: Vec<u32> = l.chars().filter_map(|c| c.to_digit(10)).collect();
-            line_vec[0] * 10 + line_vec[line_vec.len() - 1]
+            let p0 = l[..5].parse::<u32>().unwrap();
+            let p1 = l[8..].parse::<u32>().unwrap();
+            (p0, p1)
         })
-        .sum();
+        .unzip();
+
+    col1.sort_unstable();
+    col2.sort_unstable();
+
+    col1.iter().zip(col2).map(|(c1, c2)| c1.abs_diff(c2)).sum()
 }
 
 #[cfg(test)]
@@ -19,6 +26,6 @@ mod tests {
     #[test]
     fn test_day() {
         // Correct value can be put here once it is known
-        assert_eq!(54708, day());
+        assert_eq!(1_830_467, day());
     }
 }
