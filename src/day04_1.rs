d65fb175313eb048f7ca5337ad1f73efc07064ba
Riceman2000 <alex4222000@gmail.com>
2024-12-25 22:18:25 -0500

    Fix off by one for trailing newlines

--- src/day04_1.rs
+++ src/day04_1.rs
@@ -6,11 +6,11 @@ const INPUT: &[u8] = include_bytes!("../input/day04.txt");
 #[allow(clippy::cast_possible_truncation)]
 #[allow(clippy::cast_sign_loss)]
 pub fn day() -> u32 {
-    let lines: Vec<&[u8]> = INPUT.split(|c| *c == b'\n').collect();
+    let lines: Vec<&[u8]> = INPUT.trim_ascii_end().split(|c| *c == b'\n').collect();
     let mut seen = 0;
 
     // Edges will never have valid X
-    for y in 1..lines.len() - 2 {
+    for y in 1..lines.len() - 1 {
         for x in 1..lines[0].len() - 1 {
             let test_char = lines[y][x];
             if test_char != b'A' {
