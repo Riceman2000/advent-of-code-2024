e9e958dd80e2c8159809146a52c425483f75e160
Riceman2000 <alex4222000@gmail.com>
2024-12-25 22:18:25 -0500

    day 1 part 2

---
+++ src/day01_1.rs
@@ -0,0 +1,34 @@
+// Pull this file's contents into the binary as a string literal
+const INPUT: &str = include_str!("../input/day01.txt");
+
+#[must_use]
+#[allow(clippy::missing_panics_doc)]
+#[allow(clippy::cast_possible_truncation)]
+pub fn day() -> u32 {
+    let (col1, col2): (Vec<u32>, Vec<u32>) = INPUT
+        .lines()
+        .map(|l| {
+            let p0 = l[..5].parse::<u32>().unwrap();
+            let p1 = l[8..].parse::<u32>().unwrap();
+            (p0, p1)
+        })
+        .unzip();
+
+    col1.iter()
+        .map(|n| {
+            let occurances = col2.iter().filter(|n1| n == *n1).count() as u32;
+            occurances * n
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::day;
+
+    #[test]
+    fn test_day() {
+        // Correct value can be put here once it is known
+        assert_eq!(26_674_158, day());
+    }
+}
