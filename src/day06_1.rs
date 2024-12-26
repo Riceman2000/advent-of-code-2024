3d5c7e8ba90a5754ee43032738392ebc759837e1
Riceman2000 <alex4222000@gmail.com>
2024-12-25 22:18:25 -0500

    Optimize

--- src/day06_1.rs
+++ src/day06_1.rs
@@ -7,15 +7,48 @@ const INPUT: &[u8] = include_bytes!("../input/day06.txt");
 #[allow(clippy::cast_sign_loss)]
 pub fn day() -> usize {
     let lines: Vec<&[u8]> = INPUT.trim_ascii_end().split(|c| *c == b'\n').collect();
-    let mut out = 0;
+    let mut current_pos = (0, 0);
     for (y, line) in lines.iter().enumerate() {
-        for (x, char) in line.iter().enumerate() {
-            if *char == b'.' && gets_stuck((x, y)) {
-                out += 1;
-            }
+        if let Some(x) = line.iter().position(|c| *c == b'^') {
+            current_pos = (x, y);
+            break;
         }
     }
 
+    let mut direction_vectors = [(0, -1), (1, 0), (0, 1), (-1, 0)].iter().cycle();
+    let mut current_dir = direction_vectors.next().unwrap();
+
+    let mut visited = vec![current_pos];
+    'main: loop {
+        let mut next_pos;
+        loop {
+            next_pos = (
+                current_pos.0.wrapping_add_signed(current_dir.0),
+                current_pos.1.wrapping_add_signed(current_dir.1),
+            );
+            if next_pos.0 >= lines[0].len() || next_pos.1 >= lines.len() {
+                break 'main;
+            }
+            if lines[next_pos.1][next_pos.0] == b'#' {
+                current_dir = direction_vectors.next().unwrap();
+            } else {
+                break;
+            }
+        }
+
+        current_pos = next_pos;
+        visited.push(current_pos);
+    }
+
+    visited.sort_unstable();
+    visited.dedup();
+
+    let mut out = 0;
+    for (x, y) in visited {
+        if gets_stuck((x, y)) {
+            out += 1;
+        }
+    }
     out
 }
 
