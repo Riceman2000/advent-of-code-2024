7ab965c27ca8fb1305819429477500f3ab71a0d0
riceman2000 <alex4222000@gmail.com>
2025-12-01 00:54:18 -0500

    Simplify part 1

--- src/aoc2025/day01_0.rs
+++ src/aoc2025/day01_0.rs
@@ -9,40 +9,21 @@ pub struct Day;
 #[must_use]
 #[allow(clippy::missing_panics_doc)]
 pub fn day(input: &'static [u8]) -> i32 {
-    let steps: Vec<i32> = input
+    let mut out = 0;
+    input
         .trim_ascii()
         .split(|c| *c == b'\n')
-        .map(|l| unsafe {
-            let mut n = atoi(&l[1..]).unwrap_unchecked();
+        .fold(50, |mut acc, l| unsafe {
+            let mut n: i32 = atoi(&l[1..]).unwrap_unchecked();
             if l[0] == b'L' {
                 n *= -1;
             }
-            n
-        })
-        .collect();
-
-    let mut n = 50;
-    let mut out = 0;
-    for s in steps {
-        if s < 0 {
-            for _ in 0..-s {
-                n -= 1;
-                if n < 0 {
-                    n = 99;
-                }
+            acc += n;
+            acc %= 100;
+            if acc == 0 {
+                out += 1;
             }
-        } else {
-            for _ in 0..s {
-                n += 1;
-                if n > 99 {
-                    n = 0;
-                }
-            }
-        }
-        if n == 0 {
-            out += 1;
-        }
-    }
-
+            acc
+        });
     out
 }
